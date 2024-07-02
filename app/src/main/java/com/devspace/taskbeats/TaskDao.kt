81cef806b8d7c4795f7d121bbca942edb74047bf
DouglasRdosSantos <dodo010290@gmail.com>
2024-07-02 09:47:26 -0300

    Database update

--- app/src/main/java/com/devspace/taskbeats/TaskDao.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskDao.kt
@@ -23,4 +23,9 @@ interface TaskDao {
 
     @Delete
     fun delete(taskEntity: TaskEntity)
+
+    @Query("select * from taskentity where category is :categoryName")
+    fun getAllByCategoryName(categoryName: String): List<TaskEntity>
+    @Delete
+    fun deleteAll(taskEntity: List<TaskEntity>)
 }
--- app/src/main/java/com/devspace/taskbeats/TaskEntity.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskEntity.kt
@@ -2,9 +2,20 @@ package com.devspace.taskbeats
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = CategoryEntity::class,
+            parentColumns = ["key"],
+            childColumns = ["category"]
+
+        )
+    ]
+
+)
 data class TaskEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
